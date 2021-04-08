800f5d73bf5f1c880d09983e388cd7dc0c97880e
Alex M <makridenko.a@yandex.ru>
2021-04-08 13:42:12 +0300

    update settings;

--- kernel/kernel/settings.py
+++ kernel/kernel/settings.py
@@ -7,7 +7,7 @@ from .local_settings import ALLOWED_HOSTS
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG') == 'True'
 
 
 INSTALLED_APPS = [
