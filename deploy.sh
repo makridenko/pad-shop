a17508579625daaeb1b9299a9e4c96f82a62a1e0
Alex M <makridenko.a@yandex.ru>
2021-04-08 13:16:06 +0300

    change deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 docker-compose up -d --build webclient
 docker exec pad-shop_webclient_1 yarn install
 docker exec pad-shop_webclient_1 yarn build
-docker-compose down webclient
+docker-compose down
 
 rm -rf kernel/webclient
 cp -r client/build kernel/webclient
